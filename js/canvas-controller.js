75e7f2a7bc8ebf1627f2cc811f1149880f19b7bd
Ron Avnery <avnery@gmail.com>
2019-06-05 15:57:19 +0300

    Created canvas, mouse events, move by speed

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -1,14 +1,68 @@
 'use strict';
 let canvas;
 let ctx;
+let gIsMouseDown = false;
+let gIsBreakOn = false;;
+
 
 function onInit() {
     defineCanvas();
 }
 
 function defineCanvas() {
-    canvas = document.querySelector('#my-canvas');
+    canvas = document.querySelector('#our-canvas');
     ctx = canvas.getContext('2d')
     canvas.width = window.innerWidth - 100
-    canvas.height = window.innerHeight - 25
+    canvas.height = window.innerHeight - 100
+
+    canvas.addEventListener('mousedown', onMouseDown)
+    canvas.addEventListener('mousemove', onMouseMove, event)
+    canvas.addEventListener('mouseup', onMouseUp)
+}
+
+function onMouseDown() {
+    gIsMouseDown = true;
+}
+
+
+function onMouseMove(ev) {
+    if (gIsBreakOn) return;
+    let speed = getMouseTimeSpeedDis(ev);
+
+    if (gIsMouseDown) {
+        const { offsetX, offsetY } = ev;
+        let shapeWidth = speed / 10;
+        let shapeHeight = speed / 10;
+        if (speed < 250) {
+            shapeWidth = getRandomIntInclusive(30, 40);
+            shapeHeight = getRandomIntInclusive(30, 40);
+        }
+        drawRect(offsetX, offsetY, shapeWidth, shapeHeight);
+        gIsBreakOn = true;
+        setTimeout(function () { gIsBreakOn = false }, 100)
+    }
+}
+
+function onMouseUp() {
+    gIsMouseDown = false;
+}
+
+
+function draw(ev) {
+    console.log(ev);
+    // Do we need this?
+    // ctx.save() 
+    const { offsetX, offsetY } = ev;
+    console.log(offsetX, offsetY);
+    drawRect(offsetX, offsetY);
+}
+
+
+function drawRect(x, y, shapeWidth = 150, shapeHeight = 150) {
+    ctx.beginPath();
+    ctx.rect(x, y, shapeWidth, shapeHeight)
+    ctx.fillStyle = 'white'
+    ctx.fillRect(x, y, shapeWidth, shapeHeight)
+    ctx.stroke()
+    ctx.closePath();
 }
---
+++ js/canvas-util.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+
+function getMouseTimeSpeedDis(ev) {
+    let timestamp = 0;
+    let mY = 0;
+    let now = Date.now();
+    let currentmY = ev.screenY;
+    let dt = now - timestamp;
+    let distance = Math.abs(currentmY - mY);
+    let speed = Math.round(distance / dt * 1000);
+    mY = currentmY;
+    timestamp = now;
+
+    return speed;
+}
